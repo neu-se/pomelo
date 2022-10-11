a5f9f94f187fa4bb5ade7bdfd1baa99db767f026
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2022-10-11 14:15:02 -0400

    * Fixed bug where the test class was loaded before system properties were set

--- pomelo-core/src/main/java/edu/neu/ccs/prl/pomelo/scan/ScanForkMain.java
+++ pomelo-core/src/main/java/edu/neu/ccs/prl/pomelo/scan/ScanForkMain.java
@@ -19,10 +19,11 @@ public final class ScanForkMain {
     }
 
     public static void main(String[] args) throws Throwable {
+        // Set system properties before loading the test class
+        SystemPropertyUtil.load(new File(args[2]));
         Class<?> testClass = Class.forName(args[0], true, ScanForkMain.class.getClassLoader());
         String testMethodName = args[1];
         ParameterizedTestWrapper wrapper = ParameterizedTestType.wrap(new TestMethod(testClass, testMethodName));
-        SystemPropertyUtil.load(new File(args[2]));
         File report = new File(args[3]);
         GeneratorsStatus status = checkGeneratorStatus(wrapper);
         TestResult result = getTestResult(testClass, testMethodName, wrapper);
